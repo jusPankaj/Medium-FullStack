2bbd51ffd26a2446b7ab3712197f209071de51b0
pankazillion <kppankaj1843@gmail.com>
2024-06-10 16:14:16 +0530

    complted backend

---
+++ backend/src/routes/blog.ts
@@ -0,0 +1,159 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { Hono } from "hono";
+import { verify } from "hono/jwt";
+import { createBlogInput, updateBlogInput } from "@juscode/medium-common";
+
+export const blogRouter = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    SECRET_KEY: string;
+  };
+  Variables: {
+    userId: string;
+  };
+}>();
+
+// extract the userid from it
+// pass it from the m/w to actual route handler
+
+blogRouter.use("/*", async (c, next) => {
+  const authHeader = c.req.header("authorization") || "";
+  
+  try {
+    const user = await verify(authHeader, c.env.SECRET_KEY);
+    if (user) {
+      c.set("userId", user.id);
+      await next();
+    } else {
+      c.status(403);
+      return c.json({
+        message: "You are not logged in",
+      });
+    }
+  } catch (error) {
+    c.status(403);
+      return c.json({
+        message: "You are not logged in",
+      });
+  }
+});
+
+
+
+blogRouter.post("/", async (c) => {
+  const body = await c.req.json();
+
+  const { success } = createBlogInput.safeParse(body);
+  if(!success){
+    c.status(411);
+    return c.json({
+      message: "Inputs are not correct"
+    })
+  }
+
+  const authorId = c.get("userId");
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate()); // only after this .$extends(withAccelerate) prisma will be able to talk to the database
+
+  const blog = await prisma.post.create({
+    data: {
+      title: body.title,
+      content: body.content,
+      authorId: authorId,
+    },
+  });
+
+  return c.json({
+    id: blog.id,
+  });
+});
+
+
+
+
+blogRouter.put("/", async (c) => {
+  const body = await c.req.json();
+  const { success } = updateBlogInput.safeParse(body);
+  if(!success){
+    c.status(411);
+    return c.json({
+      message: "Inputs are not correct"
+    })
+  }
+
+
+  const authorId = c.get("userId");
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate()); // only after this .$extends(withAccelerate) prisma will be able to talk to the database
+
+  try {
+    const blog = await prisma.post.update({
+      where: {
+        id: body.id,
+        authorId: authorId,
+      },
+      data: {
+        title: body.title,
+        content: body.content,
+      },
+    });
+
+    return c.json({
+      id: blog.id,
+      message: "post/blog updated",
+    });
+  } catch (e) {
+    // console.log(e);
+    c.status(403);
+    c.text("Error occured while updating post");
+  }
+});
+
+
+// Todo : add pagination to this endpoint for return 10 blogs per page or on scrolling down it gets more
+blogRouter.get("/bulk", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate()); // only after this .$extends(withAccelerate) prisma will be able to talk to the database
+
+  try {
+    const blogs = await prisma.post.findMany({});
+    return c.json({ blogs });
+  } catch (error) {
+    console.error(error);
+    c.status(500)
+    return c.json({ error: "An error occurred while fetching the blogs" });
+  }
+});
+
+
+
+blogRouter.get("/:id", async (c) => {
+  const id = c.req.param("id");
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate()); // only after this .$extends(withAccelerate) prisma will be able to talk to the database
+
+  try {
+    const blog = await prisma.post.findFirst({
+      where: {
+        id: id,
+      },
+    });
+
+    return c.json({
+      blog,
+    });
+  } catch (e) {
+    c.status(411);
+    return c.json({
+      message: "Error while fetching blog post",
+    });
+  }
+});
+
+
---
+++ backend/src/routes/user.ts
@@ -0,0 +1,92 @@
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { decode, sign, verify } from "hono/jwt";
+import { signupInput, signinInput  } from "@juscode/medium-common";
+
+export const userRouter = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    SECRET_KEY: string;
+  };
+}>();
+
+userRouter.post("/signup", async (c) => {
+  // The body user is sending you must be sanitized
+  const body = await c.req.json();
+  const { success } = signupInput.safeParse(body); 
+  if(!success) {
+    c.status(411);
+    return c.json({
+      message : "Inputs not correct"
+    })
+  }
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate()); // only after this .$extends(withAccelerate) prisma will be able to talk to the database
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+
+    const jwt = await sign(
+      {
+        id: user.id,
+      },
+      c.env.SECRET_KEY
+    );
+
+    return c.text(jwt);
+  } catch (e) {
+    console.log(e);
+    c.status(411);
+    return c.text("Email already exist");
+  }
+});
+
+userRouter.post("signin", async (c) => {
+  const body = await c.req.json();
+
+  const{ success } = signinInput.safeParse(body);
+  if(!success){
+    c.status(411);
+    return c.json({
+      message : "Inputs are not correct"
+    })
+  }
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate()); // only after this .$extends(withAccelerate) prisma will be able to talk to the database
+
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+
+    if (!user) {
+      c.status(403);
+      return c.text("Invalid User");
+    }
+
+    const jwt = await sign(
+      {
+        id: user.id,
+      },
+      c.env.SECRET_KEY
+    );
+
+    return c.text("Signed in Successfully");
+  } catch (e) {
+    console.log(e);
+    c.status(411);
+    return c.text("Email already exist");
+  }
+});
